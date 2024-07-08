a74b74d3888ebb198b666f48e5d9dc844744f361
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2024-07-09 00:42:23 +0300

    adds layout and refactor favorites page

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,35 @@
+import {ReactElement, ReactNode} from 'react';
+import {Helmet} from 'react-helmet-async';
+import Header from '../header/header.tsx';
+import cn from 'classnames';
+import Footer from '../footer/footer.tsx';
+
+type LayoutProps = {
+  title: string;
+  children: ReactNode;
+  pageClass?: string;
+  mainClass?: string;
+  showFooter?: boolean;
+}
+
+function Layout({title, children, pageClass = '', mainClass = '', showFooter = false}: LayoutProps): ReactElement {
+  const pageClassNames = cn('page', pageClass);
+  const mainClassName = cn('page__main', mainClass);
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <div className={pageClassNames}>
+        <Header/>
+        <main className={mainClassName}>
+          {children}
+        </main>
+        {showFooter ? <Footer/> : null}
+      </div>
+    </>
+  );
+}
+
+export default Layout;
--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -1,24 +1,23 @@
 import {ReactElement} from 'react';
-import Header from '../../components/header/header.tsx';
-import Footer from '../../components/footer/footer.tsx';
 import FavoritesList from '../../components/favorites/favorites-list/favorites-list.tsx';
+import Layout from '../../components/layout/layout.tsx';
 
 
 function FavoritesPage(): ReactElement {
   return (
-    <div className="page">
-      <Header/>
-
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoritesList/>
-          </section>
-        </div>
-      </main>
-      <Footer/>
-    </div>
+    <Layout
+      title='Favorites'
+      pageClass='page__main--favorites'
+      mainClass='page__main--favorites'
+      showFooter
+    >
+      <div className="page__favorites-container container">
+        <section className="favorites">
+          <h1 className="favorites__title">Saved listing</h1>
+          <FavoritesList/>
+        </section>
+      </div>
+    </Layout>
   );
 }
 
